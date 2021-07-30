aa566925678087d6c56d0836bb89fabb3037e30e
Daniel Calvin <arockne2003@yahoo.com>
2021-07-30 09:02:37 -0700

    Assignment Complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+
+  inputForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const input = document.querySelector('#searchByID');
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(resp => resp.json())
+    .then(json => {
+      const title = document.querySelector('section#movieDetails h4');
+      const summary = document.querySelector('section#movieDetails p');
+      
+      title.textContent = json.title;
+      summary.textContent = json.summary;
+    })
+
+    inputForm.reset()
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
